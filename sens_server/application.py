b6dbb663268843d91985d383bd430189436c0e2a
Mark McGuire <mark.b.mcg@gmail.com>
2013-10-14 03:53:46 +0000

    Add caching and timed requests

--- sens_server/application.py
+++ sens_server/application.py
@@ -1,11 +1,16 @@
 import io
 import flask
+import flask.ext.cache
 import sens.status
 import sens.application
+import sens_server.request_handler as req_handler
 
 app = flask.Flask(__name__)
+app.config['CACHE_TYPE'] = 'simple'
+cache = flask.ext.cache.Cache(app)
 
 @app.route('/<channel_name>')
+@cache.cached(60)
 def image(channel_name):
     try:
         image = sens.application.build_image(channel_name)
@@ -20,4 +25,4 @@ def image(channel_name):
 
 def main():
     app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', request_handler=req_handler.TimedRequestHandler)
