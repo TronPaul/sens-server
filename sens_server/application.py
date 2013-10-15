5d80bf143bafdae4a39744f89742f439a266499d
Mark McGuire <mark.b.mcg@gmail.com>
2013-10-15 20:56:03 +0000

    Add logging

--- sens_server/application.py
+++ sens_server/application.py
@@ -13,13 +13,19 @@ def image(channel_name):
     try:
         image = sens.application.build_image(channel_name)
     except sens.status.ChannelNotFoundError as e:
+        app.logger.info(('Requested channel {channel_name} '
+                'not found by Twitch').format(channel_name=channel_name))
         flask.abort(404)
     except sens.status.TwitchError as e:
-        flask.abort(400)
+        app.logger.info(('Requested channel {channel_name} '
+                'recieved error response from '
+                'Twitch').format(channel_name=channel_name))
+        flask.abort(404)
     fp = io.BytesIO()
     image.save(fp, 'png')
     fp.seek(0)
     return flask.send_file(fp, mimetype='image/png', cache_timeout=60)
 
 def main():
+    app.debug=True
     app.run(host='0.0.0.0', request_handler=req_handler.TimedRequestHandler)
